6b26d399d2d7721d0002f3ad0c58af9c31adf7dc
Jhonatan Freitas <itjhonatan01@gmail.com>
2019-03-14 21:37:14 -0300

    aula ionic

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SlidePage } from '../slide/slide';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  title:string = "HTML 5";
+  
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController
+    ) {}
 
+  goToSlide(){
+    this.navCtrl.push(SlidePage);
   }
-
 }
